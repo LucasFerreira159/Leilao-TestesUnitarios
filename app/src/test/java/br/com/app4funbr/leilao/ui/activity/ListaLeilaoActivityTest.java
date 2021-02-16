ab9da591e4af94794a2919d5c7f13a88eb0413a8
LucasFerreira <tlucaskfm@fastshop.com.br>
2021-02-16 11:58:17 -0300

    Mockando resposta api

--- app/src/test/java/br/com/app4funbr/leilao/ui/activity/ListaLeilaoActivityTest.java
+++ app/src/test/java/br/com/app4funbr/leilao/ui/activity/ListaLeilaoActivityTest.java
@@ -1,20 +1,57 @@
 package br.com.app4funbr.leilao.ui.activity;
 
+import android.content.Context;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.app4funbr.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.app4funbr.leilao.api.retrofit.client.RespostaListener;
+import br.com.app4funbr.leilao.model.Leilao;
+import br.com.app4funbr.leilao.ui.recyclerview.adapter.ListaLeilaoAdapter;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ListaLeilaoActivityTest{
+
+    @Mock
+    private LeilaoWebClient client;
+    @Mock
+    private Context context;
+    @Spy
+    private ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(context);
 
     @Test
     public void deve_AtualizaListaDeLeiloes_QuandoBuscarLeiloesDaApi() throws InterruptedException {
         ListaLeilaoActivity activity = new ListaLeilaoActivity();
+        Mockito.doNothing().when(adapter).atualizaLista();
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                RespostaListener<List<Leilao>> argument = invocation.getArgument(0);
+                argument.sucesso(new ArrayList<Leilao>(Arrays.asList(
+                        new Leilao("Computador"),
+                        new Leilao("Carro")
+                )));
+                return null;
+            }
+        }).when(client).todos(ArgumentMatchers.any(RespostaListener.class));
 
-        activity.configuraAdapter();
-        activity.buscaLeiloes();
-        Thread.sleep(2000);
-        int quantidadeLeiloesDevolvida = activity.getAdapter().getItemCount();
+        activity.buscaLeiloes(adapter, client);
+        int quantidadeLeiloesDevolvida = adapter.getItemCount();
 
         assertThat(quantidadeLeiloesDevolvida, is(2));
     }
